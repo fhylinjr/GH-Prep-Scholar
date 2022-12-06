bdaeff8286505b7530d606fb33e94b5849c01fb3
Philip Boakye <philippoinzaghi10@gmail.com>
2022-12-06 04:38:52 -0300

    modified index.php

--- index.php
+++ index.php
@@ -1,5 +1,1 @@
 <?php include_once("index.html"); ?>
-<?php include_once("required docs.html"); ?>
-<?php include_once("test prep.html"); ?>
-<?php include_once("college app.html"); ?>
-<?php include_once("financial aid.html"); ?>
