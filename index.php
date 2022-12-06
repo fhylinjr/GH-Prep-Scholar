5db6c7c1d814e01b46364ce373113011db6d3a20
Philip Boakye <philippoinzaghi10@gmail.com>
2022-12-06 04:37:02 -0300

    added php

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include_once("index.html"); ?>
+<?php include_once("required docs.html"); ?>
+<?php include_once("test prep.html"); ?>
+<?php include_once("college app.html"); ?>
+<?php include_once("financial aid.html"); ?>
